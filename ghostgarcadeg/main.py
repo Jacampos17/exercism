cf70e903c6651f40827964c4b1c46f10ce8ccc5d
José Alejandro Campos Portillo <jalejandro.campos17@gmail.com>
2026-01-30 19:11:20 -0600

    jan30-pacman3

--- ghostgarcadeg/main.py
+++ ghostgarcadeg/main.py
@@ -2,20 +2,20 @@
 
 
 def eat_ghost(power_pellet_active, touching_ghost):
-    """Verify that Pac-Man can eat a ghost if he is empowered by a power pellet."""
+    """Verify that Pac-Man can eat a ghost if he is empowered by a power pellet."""  # noqa: E501
     return power_pellet_active and touching_ghost
 
 
 def score(touching_power_pellet, touching_dot):
-    """Verify that Pac-Man has scored when a power pellet or dot has been eaten."""
+    """Verify that Pac-Man has scored when a power pellet or dot has been eaten."""  # noqa: E501
     return touching_power_pellet or touching_dot
 
 
 def lose(power_pellet_active, touching_ghost):
-    """Trigger the game loop to end (GAME OVER) when Pac-Man touches a ghost without his power pellet."""
+    """Trigger the game loop to end (GAME OVER) when Pac-Man touches a ghost without his power pellet."""  # noqa: E501
     return touching_ghost and not power_pellet_active
 
 
 def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
     """Trigger the victory event when all dots have been eaten."""
-    return has_eaten_all_dots and not (touching_ghost and not power_pellet_active)
+    return has_eaten_all_dots and not (touching_ghost and not power_pellet_active)  # noqa: E501
