9b4e60a529b107e9fa7d2f701cb6d47a90eb3caf
José Alejandro Campos Portillo <jalejandro.campos17@gmail.com>
2026-01-30 19:10:25 -0600

    jan30-pacman2

--- ghostgarcadeg/main.py
+++ ghostgarcadeg/main.py
@@ -1,28 +1,21 @@
-# Define if Pac-Man eats a ghost
+"""Functions for implementing the rules of the classic arcade game Pac-Man."""
 
-def eats_ghost(power_pellet_active, touching_ghost):
-    if power_pellet_active and touching_ghost:
-        return True
-    else:
-        return False
+
+def eat_ghost(power_pellet_active, touching_ghost):
+    """Verify that Pac-Man can eat a ghost if he is empowered by a power pellet."""
+    return power_pellet_active and touching_ghost
 
 
 def score(touching_power_pellet, touching_dot):
-    if touching_power_pellet or touching_dot:
-        return True
-    else:
-        return False
+    """Verify that Pac-Man has scored when a power pellet or dot has been eaten."""
+    return touching_power_pellet or touching_dot
 
 
 def lose(power_pellet_active, touching_ghost):
-    if not power_pellet_active and touching_ghost:
-        return True
-    else:
-        return False
+    """Trigger the game loop to end (GAME OVER) when Pac-Man touches a ghost without his power pellet."""
+    return touching_ghost and not power_pellet_active
 
 
-def win(has_eaten_all_dots, touching_ghost, power_pellet_active):
-    if has_eaten_all_dots and not (touching_ghost and not power_pellet_active):
-        return True
-    else:
-        return False
+def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
+    """Trigger the victory event when all dots have been eaten."""
+    return has_eaten_all_dots and not (touching_ghost and not power_pellet_active)
